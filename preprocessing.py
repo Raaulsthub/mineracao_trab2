e9385a11355699dc9bcadef9060d6f3084b36e04
Raaulsthub <rsteinmetz@inf.ufsm.br>
2023-01-10 08:48:09 -0300

    finish preprocessing

--- preprocessing.py
+++ preprocessing.py
@@ -1,5 +1,8 @@
 import pandas as pd
 import numpy as np
+import warnings
+
+warnings.filterwarnings("ignore") # ignoring pandas data[x][y] deprecated warnings
 
 
 MEN = 0
@@ -13,6 +16,10 @@ ESTRANGEIRO = 0
 
 # importing the dataframe
 data = pd.read_csv('./data/saudeRS_2022.csv', sep=';')
+
+# PRINTING
+print('Initial Data Frame structure: ')
+print(data.info())
 
 # converting all strings to lowercase
 data = data.apply(lambda x: x.astype(str).str.lower())
@@ -39,17 +46,27 @@ aux  = np.zeros(len(data['CONDICOES'])).astype(int)
 itr = 0
 for i in data['CONDICOES']:
        if i != 'nan':
-              aux[itr] = 1
+              aux[itr] = SIM
        itr += 1
 
 data['CONDICOES'] = aux
 
-# etnia indigen will be implemented as yes or no
+# pais_nascimento will be implemented as yes or no
+data['BRASILEIRO'] = np.ones(len(data['PAIS_NASCIMENTO'])).astype(int)
+itr = 0
+for i in data['PAIS_NASCIMENTO']:
+       if i != 'brasil':
+              data['BRASILEIRO'][itr] = ESTRANGEIRO
+       itr += 1
+
+data.drop(['PAIS_NASCIMENTO'], axis=1, inplace=True)
+
+# etnia indigena will be implemented as yes or no
 aux  = np.zeros(len(data['ETNIA_INDIGENA'])).astype(int)
 itr = 0
 for i in data['ETNIA_INDIGENA']:
        if i != 'nan' and i != 'nao encontrado':
-              aux[itr] = 1
+              aux[itr] = SIM
        itr += 1
 data['ETNIA_INDIGENA'] = aux
 
@@ -59,18 +76,18 @@ data.drop(['DATA_CONFIRMACAO', 'DATA_SINTOMAS', 'DATA_INCLUSAO', 'DATA_EVOLUCAO'
 # ONE HOT ENCODING
 
 # FAIXA ETARIA
-data['IDADE_1'] = np.zeros(len(data['FAIXAETARIA']))
-data['IDADE_1_4'] = np.zeros(len(data['FAIXAETARIA']))
-data['IDADE_5_9'] = np.zeros(len(data['FAIXAETARIA']))
-data['IDADE_10_14'] = np.zeros(len(data['FAIXAETARIA']))
-data['IDADE_15_19'] = np.zeros(len(data['FAIXAETARIA']))
-data['IDADE_20_29'] = np.zeros(len(data['FAIXAETARIA']))
-data['IDADE_30_39'] = np.zeros(len(data['FAIXAETARIA']))
-data['IDADE_40_49'] = np.zeros(len(data['FAIXAETARIA']))
-data['IDADE_50_59'] = np.zeros(len(data['FAIXAETARIA']))
-data['IDADE_60_69'] = np.zeros(len(data['FAIXAETARIA']))
-data['IDADE_70_79'] = np.zeros(len(data['FAIXAETARIA']))
-data['IDADE_80'] = np.zeros(len(data['FAIXAETARIA']))
+data['IDADE_1'] = np.zeros(len(data['FAIXAETARIA'])).astype(int)
+data['IDADE_1_4'] = np.zeros(len(data['FAIXAETARIA'])).astype(int)
+data['IDADE_5_9'] = np.zeros(len(data['FAIXAETARIA'])).astype(int)
+data['IDADE_10_14'] = np.zeros(len(data['FAIXAETARIA'])).astype(int)
+data['IDADE_15_19'] = np.zeros(len(data['FAIXAETARIA'])).astype(int)
+data['IDADE_20_29'] = np.zeros(len(data['FAIXAETARIA'])).astype(int)
+data['IDADE_30_39'] = np.zeros(len(data['FAIXAETARIA'])).astype(int)
+data['IDADE_40_49'] = np.zeros(len(data['FAIXAETARIA'])).astype(int)
+data['IDADE_50_59'] = np.zeros(len(data['FAIXAETARIA'])).astype(int)
+data['IDADE_60_69'] = np.zeros(len(data['FAIXAETARIA'])).astype(int)
+data['IDADE_70_79'] = np.zeros(len(data['FAIXAETARIA'])).astype(int)
+data['IDADE_80'] = np.zeros(len(data['FAIXAETARIA'])).astype(int)
 
 itr = 0
 for i in data['FAIXAETARIA']:
@@ -104,12 +121,12 @@ for i in data['FAIXAETARIA']:
 data.drop(['FAIXAETARIA'], axis=1, inplace=True)
 
 # CRITERIO
-data['TESTE_RTPCR'] = np.zeros(len(data['CRITERIO']))
-data['TESTE_RAPIDO'] = np.zeros(len(data['CRITERIO']))
-data['TESTE_OUTRO'] = np.zeros(len(data['CRITERIO']))
-data['TESTE_CLINICO_EPI'] = np.zeros(len(data['CRITERIO']))
-data['TESTE_CLINICO_IMG'] = np.zeros(len(data['CRITERIO']))
-data['TESTE_CLINICO'] = np.zeros(len(data['CRITERIO']))
+data['TESTE_RTPCR'] = np.zeros(len(data['CRITERIO'])).astype(int)
+data['TESTE_RAPIDO'] = np.zeros(len(data['CRITERIO'])).astype(int)
+data['TESTE_OUTRO'] = np.zeros(len(data['CRITERIO'])).astype(int)
+data['TESTE_CLINICO_EPI'] = np.zeros(len(data['CRITERIO'])).astype(int)
+data['TESTE_CLINICO_IMG'] = np.zeros(len(data['CRITERIO'])).astype(int)
+data['TESTE_CLINICO'] = np.zeros(len(data['CRITERIO'])).astype(int)
 
 itr = 0
 for i in data['CRITERIO']:
@@ -131,9 +148,9 @@ data.drop(['CRITERIO'], axis=1, inplace=True)
 
 # EVOLUCAO ['RECUPERADO', 'OBITO', 'OBITO OUTRAS CAUSAS']
 
-data['EVOLUCAO_RECUPERADO'] = np.zeros(len(data['EVOLUCAO']))
-data['EVOLUCAO_OBITO'] = np.zeros(len(data['EVOLUCAO']))
-data['EVOLUCAO_OBITO_OC'] = np.zeros(len(data['EVOLUCAO']))
+data['EVOLUCAO_RECUPERADO'] = np.zeros(len(data['EVOLUCAO'])).astype(int)
+data['EVOLUCAO_OBITO'] = np.zeros(len(data['EVOLUCAO'])).astype(int)
+data['EVOLUCAO_OBITO_OC'] = np.zeros(len(data['EVOLUCAO'])).astype(int)
 
 itr = 0
 for i in data['EVOLUCAO']:
@@ -149,12 +166,12 @@ data.drop(['EVOLUCAO'], axis=1, inplace=True)
 
 
 # RACA_COR 'BRANCA', 'NAO INFORMADO', 'PARDA', 'PRETA', 'AMARELA', 'INDIGENA'
-data['RACA_COR_BRANCA'] = np.zeros(len(data['RACA_COR']))
-data['RACA_COR_NA'] = np.zeros(len(data['RACA_COR']))
-data['RACA_COR_PARDA'] = np.zeros(len(data['RACA_COR']))
-data['RACA_COR_PRETA'] = np.zeros(len(data['RACA_COR']))
-data['RACA_COR_AMARELA'] = np.zeros(len(data['RACA_COR']))
-data['RACA_COR_INDIGENA'] = np.zeros(len(data['RACA_COR']))
+data['RACA_COR_BRANCA'] = np.zeros(len(data['RACA_COR'])).astype(int)
+data['RACA_COR_NA'] = np.zeros(len(data['RACA_COR'])).astype(int)
+data['RACA_COR_PARDA'] = np.zeros(len(data['RACA_COR'])).astype(int)
+data['RACA_COR_PRETA'] = np.zeros(len(data['RACA_COR'])).astype(int)
+data['RACA_COR_AMARELA'] = np.zeros(len(data['RACA_COR'])).astype(int)
+data['RACA_COR_INDIGENA'] = np.zeros(len(data['RACA_COR'])).astype(int)
 
 itr = 0
 for i in data['RACA_COR']:
@@ -174,10 +191,30 @@ for i in data['RACA_COR']:
 
 data.drop(['RACA_COR'], axis=1, inplace=True)
 
-# dont know
-# - fonte de informacao - one hot
-# - pais nascimento - BRASILEIRO OU NAO
-# - cod regiao covid - one hot
+# FONTE DE INFORMACAO:  ['E-SUS', 'SIVEP HOSP', 'SIVEP US', nan]
+data['FONTE_SUS'] = np.zeros(len(data['FONTE_INFORMACAO'])).astype(int)
+data['FONTE_HOSP'] = np.zeros(len(data['FONTE_INFORMACAO'])).astype(int)
+data['FONTE_US'] = np.zeros(len(data['FONTE_INFORMACAO'])).astype(int)
+
+itr = 0
+for i in data['FONTE_INFORMACAO']:
+       if i == 'e-sus':
+              data['FONTE_SUS'][itr] = 1
+       elif i == 'sivep hosp':
+              data['FONTE_HOSP'][itr] = 1
+       elif i == 'sivep us':
+              data['FONTE_US'][itr] = 1
+       itr += 1 
+
+data.drop(['FONTE_INFORMACAO'], axis=1, inplace=True)
+
+
+# PRINTING
+print('Final Data Frame structure: ')
+print(data.info())
+
+# SAVING NEW DATA
+data.to_csv('./data/final.csv')
 
 
 '''                                             NOTES - all the data bellow is now in lower case
@@ -187,7 +224,7 @@ data.drop(['RACA_COR'], axis=1, inplace=True)
 
 ++ MUNICIPIO: to many values to track
 
-COD_REGIAO_COVID: [16, 14,  1, 10, 11,  2, 12, 17,  7, 15, 20, 21,  5, 13,  3, 18,  6, 9,  8,  4, 19]
+++ COD_REGIAO_COVID: [16, 14,  1, 10, 11,  2, 12, 17,  7, 15, 20, 21,  5, 13,  3, 18,  6, 9,  8,  4, 19]
 
 ++ REGIAO_COVID:['BAGE - R22', 'PASSO FUNDO - R17 R18 R19', 'SANTA MARIA - R01 R02',
        'IJUI - R13', 'SANTA ROSA - R14', 'URUGUAIANA - R03',
@@ -224,24 +261,24 @@ COD_REGIAO_COVID: [16, 14,  1, 10, 11,  2, 12, 17,  7, 15, 20, 21,  5, 13,  3, 1
 
 ++ CONDICOES: ['nan', 'unique combinations']
 
-++ GESTANTE: ['NAO', 'SIM' + lots of inconsistencies]
+++ GESTANTE: ['NAO', 'SIM']
 
 ++ DATA_INCLUSAO_OBITO: unique values
 ++ DATA_EVOLUCAO_ESTIMADA: unique values
 
 ++ RACA_COR: ['BRANCA', 'NAO INFORMADO', 'PARDA', 'PRETA', 'AMARELA', 'INDIGENA']
 
-++ INDIGENA: [tribes names + lots of inconsistencies]
+++ INDIGENA: [tribes names, 'nan', 'nao encontrado']
 
-++ PROFISSIONAL_SAUDE: ['SIM', 'NAO' + lots of inconsistencies]
+++ PROFISSIONAL_SAUDE: ['NAO', 'NAO INFORMADO', 'SIM'
 
 ++ BAIRRO: neighborhood names, semi-unique values
 
-++ SRAG: ['SIM', 'NAO' + lots of inconsistencies]
+++ SRAG: ['NAO', 'SIM']
 
-FONTE_INFORMACAO: ['E-SUS', 'SIVEP HOSP' + lots of inconsistencies]
+++ FONTE_INFORMACAO: ['E-SUS', 'SIVEP HOSP', 'SIVEP US', nan]
 
-PAIS_NASCIMENTO: [countries names + lots of inconsistencies]
+++ PAIS_NASCIMENTO: [countries names]
 
-++ PES_PRIV_LIBERDADE: ['SIM', 'NAO' + lots of inconsistencies]
+++ PES_PRIV_LIBERDADE: ['SIM', 'NAO']
 '''
