69159fe857170022197b7cf7d20e70fd52b4cf5d
Joao <jdrmazzarolo@outlook.com>
2023-01-12 17:12:25 -0300

    Simplifying

---
+++ simpleApriori.py
@@ -0,0 +1,28 @@
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+from mlxtend.frequent_patterns import apriori, association_rules
+
+
+# importing data frame
+df = pd.read_csv('./data/simpleDF.csv')
+
+# **** NEED TO TAKE OUT NON NUMERIC ROWS TO RUN APRIORI ****
+df.drop(['Unnamed: 0'], axis=1, inplace=True)
+
+print(df.info())
+
+# Building the model
+frq_items = apriori(df, min_support = 0.01, use_colnames = True)
+
+# Collecting the inferred rules in a dataframe
+rules = association_rules(frq_items, metric ="lift", min_threshold = 1)
+rules = rules.sort_values(['confidence', 'lift'], ascending =[False, False])
+index = np.arange(len(rules.index))
+rules['index'] = index
+rules.set_index('index', inplace=True)
+rules.to_csv("./data/rules.csv")
+
+# printing rules
+print("RULES FOUND: ")
+print(rules.head(100)) 
---
+++ simplifying.py
@@ -0,0 +1,18 @@
+import numpy as np
+import pandas as pd
+
+df = pd.read_csv('./data/final.csv')
+
+# **** NEED TO TAKE OUT NON NUMERIC ROWS TO RUN APRIORI ****
+df.drop(['Unnamed: 0', 'COD_IBGE', 'MUNICIPIO', 'COD_REGIAO_COVID', 'REGIAO_COVID', 'BAIRRO',
+        'TESTE_RTPCR', 'TESTE_RAPIDO', 'TESTE_OUTRO', 'TESTE_CLINICO_EPI', 'TESTE_CLINICO_IMG',
+        'TESTE_CLINICO', 'PES_PRIV_LIBERDADE', 'BRASILEIRO', 'RACA_COR_BRANCA', 'RACA_COR_NA',
+        'RACA_COR_PARDA', 'RACA_COR_PRETA', 'RACA_COR_AMARELA', 'RACA_COR_INDIGENA', 'FONTE_SUS',
+        'FONTE_HOSP', 'FONTE_US'], axis=1, inplace=True)
+
+df = df.astype(bool)
+
+print(df.info())
+
+# SAVING NEW DATA
+df.to_csv('./data/simpleDF.csv')
